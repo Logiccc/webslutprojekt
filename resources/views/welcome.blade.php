c5b3d37405db2b6ec2e59f36280063cab213c921
David Persson <te16.david.persson@umea.nti.se>
2019-05-23 11:11:51 +0200

    Printar ut vapen iaf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <img src="logga2.jgp" alt="varför är det ingen bild här?">
 </head>
 <body>
-    
+    <?php
+        $servername = "localhost";
+        $username = "david";
+        $password = "persson";
+        $dbname = "slutProjekt";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "SELECT id, name, vapensort FROM vapen ORDER BY vapensort ASC";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "id: " . $row["id"]. " - name: " . $row["name"]. " " . $row["vapensort"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+
+        
+?>
 
 </body>
 </html>
